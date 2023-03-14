7dd10abc786e55341b028a57ef574061f1f80390
WumaCoder <15804854160@163.com>
2023-03-14 11:27:44 +0800

    test: add case

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
-const { encodeUtf8mb3, decodeUtf8mb3, includeEncodeUtf8mb3 } = require(".");
+const {
+  encodeUtf8mb3,
+  decodeUtf8mb3,
+  includeEncodeUtf8mb3,
+  includeUtf8mb4,
+} = require(".");
 
 const str = "汉😊😊🛝🛝🛝汉";
-// const str = "😊🛝🛝";
+// const str = "😊";
 
 console.log(str, str.length);
 
 const utf8mb3 = encodeUtf8mb3(str);
 
 console.log("encodeUtf8mb4", utf8mb3, utf8mb3.length);
-console.log("decodeUtf8mb4", decodeUtf8mb3(utf8mb3));
-console.log("include", includeEncodeUtf8mb3(utf8mb3));
-console.log("include", includeEncodeUtf8mb3("测试"));
+console.log("decodeUtf8mb4", decodeUtf8mb3(utf8mb3)); // 重复decode结果不会变
+console.log("decodeUtf8mb4", decodeUtf8mb3(decodeUtf8mb3(utf8mb3))); // 重复decode结果不会变
+console.log("includeEncodeUtf8mb3", includeEncodeUtf8mb3(utf8mb3));
+console.log("includeEncodeUtf8mb3", includeEncodeUtf8mb3(str));
+console.log("includeUtf8mb4", includeUtf8mb4(str));
+console.log("includeUtf8mb4", includeUtf8mb4("测试"));
